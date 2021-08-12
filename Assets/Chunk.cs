7e04d5e6edae4674371117a15649cc3e32dfd3cc
91i4ch <iosifov.p.v@gmail.com>
2021-08-12 19:15:12 +0300

    34/50

--- Assets/Chunk.cs
+++ Assets/Chunk.cs
@@ -2,51 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chunk : MonoBehaviour {
+public class Chunk {
 
 	public Material cubeMaterial;
-	public Block[,,] chunkData = null;
+	public Block[,,] chunkData;
+	public GameObject chunk;
 
-	IEnumerator BuildChunk(int sizeX, int sizeY, int sizeZ)
+	void BuildChunk()
 	{
-		chunkData = new Block[sizeX,sizeY,sizeZ];
-		for(int z = 0; z < sizeZ; z++)
-			for(int y = 0; y < sizeY; y++)
-				for(int x = 0; x < sizeX; x++)
+		chunkData = new Block[World.chunkSize,World.chunkSize,World.chunkSize];
+
+		for(int z = 0; z < World.chunkSize; z++)
+			for(int y = 0; y < World.chunkSize; y++)
+				for(int x = 0; x < World.chunkSize; x++)
 				{
 					Vector3 pos = new Vector3(x,y,z);
-					if(Random.Range(0,100)<50){
-						chunkData[x,y,z] = new Block(Block.BlockType.DIRT, pos, this.gameObject, cubeMaterial);
+					int worldX = (int)(x + chunk.transform.position.x);
+					int worldY = (int)(y + chunk.transform.position.y);
+					int worldZ = (int)(z + chunk.transform.position.z);
+					
+					
+					
+					if(Utils.fBM3D(worldX, worldY, worldZ, 0.1f, 3) < 0.42f)
+						chunkData[x,y,z] = new Block(Block.BlockType.AIR, pos, 
+						                chunk.gameObject, this);
+					else if(worldY <= Utils.GenerateStoneHeight(worldX,worldZ))
+					{
+						if(Utils.fBM3D(worldX, worldY, worldZ, 0.01f, 2) < 0.4f && worldY < 40)
+							chunkData[x,y,z] = new Block(Block.BlockType.DIAMOND, pos, 
+						                chunk.gameObject, this);
+						else
+							chunkData[x,y,z] = new Block(Block.BlockType.STONE, pos, 
+						                chunk.gameObject, this);
 					}
-					else{
-						chunkData[x,y,z] = new Block(Block.BlockType.AIR, pos, this.gameObject, cubeMaterial);
-					}
+					else if(worldY == Utils.GenerateHeight(worldX,worldZ))
+						chunkData[x,y,z] = new Block(Block.BlockType.GRASS, pos, 
+						                chunk.gameObject, this);
+					else if(worldY < Utils.GenerateHeight(worldX,worldZ))
+						chunkData[x,y,z] = new Block(Block.BlockType.DIRT, pos, 
+						                chunk.gameObject, this);
+					else
+						chunkData[x,y,z] = new Block(Block.BlockType.AIR, pos, 
+						                chunk.gameObject, this);
 				}
-		for(int z = 0; z < sizeZ; z++)
-			for(int y = 0; y < sizeY; y++)
-				for(int x = 0; x < sizeX; x++)
+	}
+
+	public void DrawChunk()
+	{
+		for(int z = 0; z < World.chunkSize; z++)
+			for(int y = 0; y < World.chunkSize; y++)
+				for(int x = 0; x < World.chunkSize; x++)
 				{
-					chunkData[x,y,z].Draw();
-					//b.Draw();
+					chunkData[x,y,z].Draw();	
 				}
 		CombineQuads();
-		yield return null;
 	}
 
 	// Use this for initialization
-	void Start () {
-		StartCoroutine(BuildChunk(4,4,4));
+	public Chunk (Vector3 position, Material c) {
+		
+		chunk = new GameObject(World.BuildChunkName(position));
+		chunk.transform.position = position;
+		cubeMaterial = c;
+		BuildChunk();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	void CombineQuads()
 	{
 		//1. Combine all children meshes
-		MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+		MeshFilter[] meshFilters = chunk.GetComponentsInChildren<MeshFilter>();
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
         int i = 0;
         while (i < meshFilters.Length) {
@@ -56,19 +80,19 @@ public class Chunk : MonoBehaviour {
         }
 
         //2. Create a new mesh on the parent object
-        MeshFilter mf = (MeshFilter) this.gameObject.AddComponent(typeof(MeshFilter));
+        MeshFilter mf = (MeshFilter) chunk.gameObject.AddComponent(typeof(MeshFilter));
         mf.mesh = new Mesh();
 
         //3. Add combined meshes on children as the parent's mesh
         mf.mesh.CombineMeshes(combine);
 
         //4. Create a renderer for the parent
-		MeshRenderer renderer = this.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+		MeshRenderer renderer = chunk.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
 		renderer.material = cubeMaterial;
 
 		//5. Delete all uncombined children
-		foreach (Transform quad in this.transform) {
-     		Destroy(quad.gameObject);
+		foreach (Transform quad in chunk.transform) {
+     		GameObject.Destroy(quad.gameObject);
  		}
 
 	}
